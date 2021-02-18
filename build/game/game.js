35d135dba8eb088a19787374a3321d32ad848fe5
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-02-18 21:06:49 +0100

    i

--- build/game/game.js
+++ build/game/game.js
@@ -1,5 +1,9 @@
 let myGame = new Game('myGame');
 let player1 = new Player(new Position(0, 0), ID.player);
+let player2 = new Player(new Position(200, 0), ID.player);
+let player3 = new Player(new Position(200, 200), ID.player);
 myGame.handler.addObject(player1);
+myGame.handler.addObject(player2);
+myGame.handler.addObject(player3);
 myGame.start();
 //# sourceMappingURL=game.js.map
